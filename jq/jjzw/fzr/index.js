68bc049aa26d1a65c4c38e10796fd2530c179ed0
kevin1987 <284731580@qq.com>
2017-10-20 22:51:48 +0800

    添加按钮控制

--- jq/jjzw/fzr/index.js
+++ jq/jjzw/fzr/index.js
@@ -1,5 +1,13 @@
 $(function(){
 
+    $(".ui-collapsible-heading").on("click",function () {
+        if(  $(this).parent().hasClass("ui-collapsible-collapsed")  ){
+            $( "#"+$(this).parent().attr("val") ).show();
+        }else{
+            $( "#"+$(this).parent().attr("val") ).hide();
+        }
+    });
+
 });
 
 
